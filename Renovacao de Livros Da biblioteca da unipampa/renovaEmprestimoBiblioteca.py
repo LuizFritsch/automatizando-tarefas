89944f06c7cfb0ebc71a8944a1d247ceb5d42756
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2018-10-16 12:22:18 -0300

    Renovação de livros da unipampa
    Este script renova todos os livros que você tem reservados da biblioteca da Unipampa ;)

---
+++ Renovacao de Livros Da biblioteca da unipampa/renovaEmprestimoBiblioteca.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://guri.unipampa.edu.br/ptl/sistema/showLogin")
+elem = driver.find_element_by_name("login")
+elem.clear()
+elem.send_keys("Seu login aqui")
+time.sleep(1)
+elem = driver.find_element_by_name("senha")
+elem.clear()
+elem.send_keys("Sua senha aqui")
+elem = driver.find_element_by_id("entrar").click()
+time.sleep(1)
+driver.get("https://guri.unipampa.edu.br/bib/biblioteca/consultarSituacao/")
+driver.find_element_by_id("cbAllEmp").click()
+time.sleep(1)
+driver.find_element_by_xpath('//*[@title="Renovar"]').click()
+self.assertTrue(True)
+driver.close()
