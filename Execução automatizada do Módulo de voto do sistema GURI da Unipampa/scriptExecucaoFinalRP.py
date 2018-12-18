4ed1ea137495a5d92b91e5aad0c9e7f2de6fa454
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2018-12-18 10:27:06 -0200

    Update scriptExecucaoFinalRP.py

--- Execução automatizada do Módulo de voto do sistema GURI da Unipampa/scriptExecucaoFinalRP.py
+++ Execução automatizada do Módulo de voto do sistema GURI da Unipampa/scriptExecucaoFinalRP.py
@@ -11,16 +11,61 @@ from selenium.webdriver.common.keys import Keys
 def executa(tipoUsuario):
 	if tipoUsuario == "Moderador":
 		driver = webdriver.Chrome()
-		driver.get("http://localhost/sistema/sistemaVoto/login")
-		driver.set_window_size(808, 443)
-		time.sleep(8)
-
-	else:
-		#Tem que ver se ele reseta tudo
-		driver = webdriver.Chrome()
-		driver.get("http://localhost/sistema/sistemaVoto/login")	
+		driver.get("http://localhost/sistema/sistemaVoto/")
 		driver.set_window_size(808, 886)
-		time.sleep(8)
+		time.sleep(5)
+		driver.find_element_by_xpath("//*[@id='div_login']/input[1]").send_keys("renaro")
+		driver.find_element_by_id('login').click()
+		#time.sleep(10)
+		driver.get("http://localhost/sistema/sistemaVoto/reunioes")
+		time.sleep(5)
+		driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/table/tbody/tr[2]/td[9]/input').click()
+		time.sleep(10)
+		driver.find_element_by_xpath('//*[@id="vtab1"]/div[3]/form/div[3]/button').click()
+		time.sleep(10)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		time.sleep(1)
+		driver.get("http://localhost/sistema/sistemaVoto/votacao/esperamod")
+		content = driver.find_element_by_class_name('btn btn-success').click()
+		time.sleep(10)
+		driver.close()
+		
+	else:
+		driver = webdriver.Chrome()
+		driver.get("http://localhost/sistema/sistemaVoto/")	
+		driver.set_window_size(808, 886)
+		time.sleep(10)
+		driver.find_element_by_xpath("//*[@id='div_login']/input[1]").send_keys("rafael")
+		driver.find_element_by_id('login').click()
+		time.sleep(3)
+		driver.get("http://localhost/sistema/sistemaVoto/reunioes")	
+		time.sleep(3)
+		driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/table/tbody/tr/td[9]/input").click()
+		time.sleep(10)
+		alert = driver.switch_to_alert()
+		alert.accept()
+		time.sleep(2)
+		driver.find_element_by_xpath("//input[@value='Abstenção']").click()
+		time.sleep(2)
+		driver.find_element_by_id('bt_votar').click()
+		time.sleep(2)
+		alert = driver.switch_to_alert()
+		alert.accept()
+		time.sleep(10)
+
+
+
+		driver.close()
+		
+
 
 t = threading.Thread(target=executa,args=("Moderador",))
 t.start()
